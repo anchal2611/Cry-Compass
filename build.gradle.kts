d1c62a288e64a570418a913408221d6aa2bec04e
Akrawat07 <rawatuma301@gmail.com>
2025-04-15 23:59:04 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,6 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
 }
+implementation("com.squareup.okhttp3:okhttp:4.9.3")
+implementation("com.squareup.retrofit2:retrofit:2.9.0")
+implementation("com.squareup.retrofit2:converter-gson:2.9.0")
